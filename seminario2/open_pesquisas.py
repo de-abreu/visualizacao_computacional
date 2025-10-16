3717a784c3cf3aabaeaf888a3bf6e18ce1554f3e
Miguel <miguelprates.ferreira@gmail.com>
2025-10-16 17:19:12 -0300

    dados de todos os professores

--- seminario2/open_pesquisas.py
+++ seminario2/open_pesquisas.py
@@ -1,13 +1,13 @@
 from utils import recover_backup
 
-DATA_PATH = 'data/professores.pkl' ## salva o backup
+DATA_PATH = 'data/professores_all.pkl' ## salva o backup
 
 professores = recover_backup(DATA_PATH) ## relaciona cada professor como nome:(listaartigos,listapesquisa)
                                         # artigo = str,  pesquisa = namedtuple Pesquisa
                                         
-for name,(artigos,pesquisas) in professores.items():
-    print(f"{name} ",'-'*(80-len(name)))
+for i,(name,(artigos,pesquisas)) in enumerate(professores.items()):
+    print(f"{i:3} {name} ",'-'*(80-len(name)))
     print(" "*6,f"{len(artigos)} artigos encontrados.")
-    print(" "*6,f"{len(pesquisas)} pesquisas encontradas")
+    print(" "*6,f"{len(pesquisas)} pesquisas encontradas. ")
     print("\n")
     
