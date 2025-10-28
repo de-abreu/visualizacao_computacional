a0b9e24ddb8d99d2fc90bc885024101a05a4c09d
Abreu <87032834+de-abreu@users.noreply.github.com>
2025-10-27 22:59:21 -0300

    fix(main): Solved the issue with size restrictions when installing sentence-transformer

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   packages = with pkgs; [
     chromedriver
     chromium
@@ -14,4 +18,8 @@
       requirements = ./requirements.txt;
     };
   };
+  # Due to size restrictions of the /tmp folder, we need to modify the folder
+  # pip uses to install "sentence-transformer", a dependency listed in
+  # requirements.txt.
+  env.TMPDIR = "${config.devenv.runtime}";
 }
